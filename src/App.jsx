d4063db5cee90658b4fea475cc05fb9ebe8de896
Marco <m.tannoia@studenti.poliba.it>
2025-11-20 19:54:12 +0100

    introduzione auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Authenticator } from '@aws-amplify/ui-react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div style={{ padding: 20 }}>
+          <h1>Ciao {user?.username}</h1>
+          <button onClick={signOut}>Logout</button>
+        </div>
+      )}
+    </Authenticator>
+  );
 }
 
-export default App
+export default App;
