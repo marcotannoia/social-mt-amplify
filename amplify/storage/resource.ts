68bd0bb6f7778aa89886afa1e26e2d1f9372896e
Marco <m.tannoia@studenti.poliba.it>
2025-11-22 22:18:29 +0100

    versione 1.0: pubblicazione disponibile

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,9 +3,10 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'postImages',
   access: (allow) => ({
-    'public/': [
+    // DEVE finire con "/*"
+    'public/*': [
       // utenti autenticati possono leggere/scrivere immagini
-      allow.authenticated.to(['read', 'write']),  // praticamente tutti i NON owner possono fare queste azioni
+      allow.authenticated.to(['read', 'write']),
     ],
   }),
 });
